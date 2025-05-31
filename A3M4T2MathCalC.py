fbc53655daeb810fcdb3e5bf6b0e155b9317a2ff
PythonJockey <familyequityclub@gmail.com>
2025-05-31 20:46:41 +0530

    Add files via upload
    Assignment 3 Mod 4 Task 2 added

---
+++ A3M4T2MathCalC.py
@@ -0,0 +1,12 @@
+import math
+# Ask for user input
+myNum = int(input('Please enter a number : '))
+
+# Square Root
+print (f'Square root of {myNum} : ', math.sqrt(myNum))
+
+#log base e
+print (f'Logarithm of {myNum} : ', math.log(myNum))
+
+# sine (in rad)
+print (f'Sine of {myNum} : ', math.sin(myNum))
